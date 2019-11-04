cd003177263687533ab5b3ca7df818adca863154
Turgerel <jessica@Jessicas-MacBook-Air.local>
2019-11-03 21:06:15 -0700

    database update

--- SD1-P1/SD1Dictionary.py
+++ SD1-P1/SD1Dictionary.py
@@ -1,83 +1,82 @@
 """
 Program Title: SD1-P1
-Program Desc.: This program implements the specifications of Senior Design (SD) Part 1, which parses nmap results 
+Program Desc.: This program implements the specifications of Senior Design (SD) Part 1, which parses nmap results
 (Dhivahari, Turgerel, Hosna) and prepares them for database transfer (Katerina, Kulprawee).
-Authors: Turgerel Amgalanbaatar, Hosna Zulali, Dhivahari Vivekanandasarma, Kulprawee Prayoonsuk, Katerina Walter
+Authors: Dhivahari Vivek, Turgerel Amgalanbaatar, Hosna Zulali, Kulprawee Prayoonsuk, Katerina Walter
 """
 import os
 import sys
 import nmap
 import json
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 try:
-    menu = int(input("\nMake a selection:\n1. Scan a single IP address.\n2. CURRENTLY UNAVAILABLE - Scan a range of IP addresses.\n3. Scan different IP addresses.\n\n"))
+    menu = int(input("\nMake a selection:\n1. Scan a single IP address.\n2. Scan a range of IP addresses.\n3. Scan different IP addresses.\n\n"))
+
+    nm = nmap.PortScanner()
+
     # scanning single IP
     if(menu == 1):
-        nm = nmap.PortScanner()
-        ip = input("\nEnter the IP to be scanned: ")
+        ip = str(input("\nEnter the IP to be scanned: "))
         print("scanning single IP address...")
-        # BELOW, THE SCANS CONDUCTED
-        nm.scan(ip, '21-443')   # port scanning user-entered IP from ports 21-443
-        nm.scan(ip, arguments='-O')     # OS detection using TCP/IP stack fingerprinting
+
     # scanning a range of IP
     elif(menu == 2):
-        nm = nmap.PortScannerAsync()
-        # this function is needed for async scanning
-        def callback_result(host, scan_result):
-            print('-------------------------------')
-            print(host, scan_result)
-        ip = input("\nEnter the range of IP addresses (example: 192.168.1.0/30): ")
+        ip = input("\nEnter the range of IP addresses via CIDR notation (example: 192.168.1.0/30): ")
         print("scanning given range of IP addresses...")
-        nm.scan(ip,'21-443')
-        nm.scan(ip, arguments='-O', callback=callback_result)
+
     # scanning different IPs
     elif(menu == 3):
-        nm = nmap.PortScanner()
         ip = input("\nEnter the IP addresses to be scanned (EACH SEPARATED BY A SINGLE SPACE): ")
         print("scanning entered IP addresses...")
-        # BELOW, THE SCANS CONDUCTED
-        nm.scan(ip, '21-443')   
-        nm.scan(ip, arguments='-O')
 
-    # creating list of nmap dictionaries
+    # running scans based on IP addresses user entered
+    nm.scan(ip, '0-443', arguments='-O')
+
+    # creating list of DOCUMENTS
+    print("Consolidating data...")
     nmapList = []
-    index = 0
     for count in nm.all_hosts():
-        nmapOBJ = {}    # creating an empty dictionary for each host
-        nmapOBJ = {'host_name':nm[count].hostname(),
-                'host_state':nm[count].state(),
-                'os_accuracy':"",
-                'os_family':"",
-                'os_generation':""
-                }
-        nmapList.append(nmapOBJ)
-        index+=1
+    
+        nmapOBJ = {}    # creating empty dictionary for each host
+        # data must be parsed differently depending on whether OS info can be collected
+        #TODO: as info list expands, change to (IF NULL DONT ADD KEYVALUE PAIR)
+        #nmapOBJ['host_IP']=count
+        nmapOBJ['host_name']=nm[count].hostname()
+        nmapOBJ['host_state']=nm[count].state()
+        nmapOBJ['all_protocols']=nm[count].all_protocols()
+        nmapOBJ['tcp_open']=nm[count].all_tcp()
+        nmapOBJ['udp_open']=nm[count].all_udp()
+        nmapOBJ['ip_open']=nm[count].all_ip()
+        nmapOBJ['sctp_open']=nm[count].all_sctp()
+        if 'osmatch' in nm[count]:
+            for osmatch in nm[count]['osmatch']:
+                nmapOBJ['os_type']=osmatch['osclass'][0]['type']
+                nmapOBJ['os_vend']=osmatch['osclass'][0]['vendor']
+                nmapOBJ['os_fam']=osmatch['osclass'][0]['osfamily']
+                nmapOBJ['os_gen']=osmatch['osclass'][0]['osgen']
         
-    index = 0
-    if 'osclass' in nm[ip]:
-        for osclass in nm[ip]['osclass']:
-            nmapList[index]['os_accuracy'] = osclass['accuracy']
-            nmapList[index]['os_family'] = osclass['osfamily']
-            nmapList[index]['os_generation'] = osclass['osgen']
-
+        mach_doc = {'hostIP' : count, 'scanData' : nmapOBJ}
+        
+        nmapList.append(mach_doc)
+        #END TODO SECTION
     
-    # preparing for JSON extraction
-    machine = {}
-    # iterating through nmapList to convert each node's data to JSON
-    index = 0
-    for node in nmapList:
-        machine[nmapList[index]['host_name']]={
-                'host_state': nmapList[index]['host_state'],
-                'os_accuracy': nmapList[index]['os_accuracy'],
-                'os_family': nmapList[index]['os_family'],
-                'os_generation': nmapList[index]['os_generation']
-                }
+    #nmapList is list of Documents (hostIP, Values<dict>) to import
+except Exception as e:
+    print(e)
 
-    s = json.dumps(machine)
+try:
     
-    # formally creating json file to be used for database entry
-    with open("nmap_nodes.json","w") as f:
-      f.write(s)
-
-except Exception as e: 
-  print(e)
+    #***Send to DB***#
+    print("Connecting to database...")
+    myclient = MongoClient("mongodb+srv://dbAdmin:dbAdmin123@scandata-fjmex.mongodb.net/test?retryWrites=true&w=majority")
+    mydb=myclient.ScanData #database
+    mycol = mydb.nmapData #collection
+    print("Sending to database...")
+    result = mycol.insert_many(nmapList)
+    #result = mycol.insert_one(nmapList[0])
+    print(result)
+    
+except Exception as e:
+    print(e)
